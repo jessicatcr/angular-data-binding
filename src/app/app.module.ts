7b5157eae82af97c313ded7bbf593205fd5c5311
juliaabud <julia.abud@enacom.com.br>
2018-07-12 14:51:52 -0300

    até input/output property

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import {FormsModule} from '@angular/forms';
+import { InputPropertyComponent } from './input-property/input-property.component';
+import { OutputPropertyComponent } from './output-property/output-property.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    InputPropertyComponent,
+    OutputPropertyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -4,6 +4,14 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
   styleUrls: ['./data-binding.component.css']
+  // styles: [ //Caso sejam poucas linhas, ai não preciso da linha de cima.
+  //   `
+  //     .highlight { /*classe CSS */
+  //       background-color: yellow;
+  //       font-weight: bold;
+  //     }
+  //   `
+  // ]
 })
 export class DataBindingComponent implements OnInit {
 
@@ -11,12 +19,45 @@ export class DataBindingComponent implements OnInit {
   cursoAngular: boolean = true; // Pode tipar ou não
   urlImagem = 'http://lorempixel.com/400/200/animals';
 
+  valorAtual: string = '';
+  valorSalvo = '';
+
+  isMouseOver: boolean = false;
+
+  nome: String = "abc";
+
+  pessoa: any = {
+    nome: 'def',
+    idade: 20
+  }
+
+  nomeDoCurso: string = 'Angular'
+
+  valorInicial = 15;
+
   getValor() {
     return 1;
   }
 
   getCurtirCurso() {
     return true;
+  }
+
+  botaoClicado() {
+    alert('Botão clicado');
+  }
+  onKeyUp(evento: KeyboardEvent) {
+    //console.log(evento);
+    //console.log((<HTMLInputElement>evento.target).value);
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
