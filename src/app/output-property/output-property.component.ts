57e5fb2daf541d1bf65a6f26af0b56b7cc768643
juliaabud <julia.abud@enacom.com.br>
2018-07-13 09:32:22 -0300

    -

--- src/app/output-property/output-property.component.ts
+++ src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -7,13 +7,16 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class OutputPropertyComponent implements OnInit {
   @Input() valor: number = 0;
+  @Output() mudouValor = new EventEmitter();
 
   incrementa() {
     this.valor++;
+    this.mudouValor.emit({novoValor: this.valor});
   }
 
   decrementa() {
     this.valor--;
+    this.mudouValor.emit({novoValor: this.valor});
   }
 
   constructor() { }
